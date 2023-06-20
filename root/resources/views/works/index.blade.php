f28b57f9c6224976d0bd824e7d42e63a23d31379
ShujiYamamoto <shuji.yamamoto@epkotsoftware.co.jp>
2023-06-20 13:18:29 +0900

    日付＆曜日（仮）

--- root/resources/views/works/index.blade.php
+++ root/resources/views/works/index.blade.php
@@ -8,6 +8,19 @@
 </head>
 <body>
     <h1>勤怠一覧</h1>
-
+    <table>
+        <thead>
+            <th>日付</th>
+            <th>曜日</th>
+        </thead>
+        <tbody>
+            @foreach ($dates as $date)
+                <tr>
+                    <td>{{ $date->format('m-d') }}</td>
+                    <td>{{ $date->format('l') }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
